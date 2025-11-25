169d7a62692c25dd523d553be3c0217ee1505e64
ushapriya12 <167428450+ushapriya12@users.noreply.github.com>
2025-11-25 10:16:29 +0530

    Update recipe_search_tool.py

--- tools/recipe_search_tool.py
+++ tools/recipe_search_tool.py
@@ -1,1 +1,8 @@
+def recipe_search(meal_name):
+    print(f"      > Searching recipe for: {meal_name}")
+    # Dummy recipe data
+    return {
+        "name": meal_name,
+        "ingredients": ["Salt", "Oil", "Veggies"]
+    }
 
