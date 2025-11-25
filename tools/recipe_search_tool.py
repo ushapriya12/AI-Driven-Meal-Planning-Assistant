f2c9d2b3b33c7557a7dc862c41e5cbc4d869e781
ushapriya12 <167428450+ushapriya12@users.noreply.github.com>
2025-11-25 10:44:22 +0530

    Delete tools directory

--- tools/recipe_search_tool.py
+++
@@ -1,8 +0,0 @@
-def recipe_search(meal_name):
-    print(f"      > Searching recipe for: {meal_name}")
-    # Dummy recipe data
-    return {
-        "name": meal_name,
-        "ingredients": ["Salt", "Oil", "Veggies"]
-    }
-
