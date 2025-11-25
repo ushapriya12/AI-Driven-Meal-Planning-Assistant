de100124458895b7e2fcf7a1327ce8e6df10ab8b
ushapriya12 <167428450+ushapriya12@users.noreply.github.com>
2025-11-25 10:13:24 +0530

    Create recipe_search_tool.py

---
+++ agents/tools/recipe_search_tool.py
@@ -0,0 +1,7 @@
+def recipe_search(meal_name):
+    print(f"      > Searching recipe for: {meal_name}")
+    # Dummy recipe data
+    return {
+        "name": meal_name,
+        "ingredients": ["Salt", "Oil", "Veggies"]
+    }
